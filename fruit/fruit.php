47d39eeb292ad548d6956cdce5ad46de6db650b8
Arakaza <aarakaza7@gmail.com>
2020-07-25 09:54:06 +0200

    add class mere fruit

--- fruit/fruit.php
+++ fruit/fruit.php
@@ -1,14 +1,32 @@
 <?php
-
+namespace oop\fruit\fruit;
 class Fruit{
 
     private $nom;
 public function __construct($nom)
 {
+    $this->nom=$nom;
    
 }
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
 
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
 
+        return $this;
+    }
 }
 
 ?>
