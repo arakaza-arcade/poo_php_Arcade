f3396195c7a1891297d91ad29a5a6d754207863f
Arakaza <aarakaza7@gmail.com>
2020-07-25 10:05:51 +0200

    add class fille herite la classe mère fruit

---
+++ fruit/anana.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace oop\fruit\anana;
+use oop\fruit\fruit\fruit;
+
+class Anana extends fruit{
+
+private $poids=5;
+public function __construct($nom)
+{
+    parent::__construct($nom);
+}
+
+/**
+ * Get the value of poids
+ */ 
+public function getPoids()
+{
+return $this->poids;
+}
+
+public function affichage()
+{
+    echo 'le nom du fruit est  '.$this->getNom(). 'avec le poids ' .$this->getNom();
+}
+}
