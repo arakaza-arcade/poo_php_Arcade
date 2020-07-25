2662bc833d7715ead0fb8b0475df98115fc2c3e7
Arakaza <aarakaza7@gmail.com>
2020-07-25 10:29:39 +0200

    affichage des fruits

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+use oop\fruit\Anana\anana;
+use oop\fruit\orange\Orange;
+
+include('fruit/anana.php');
+include('fruit/orange.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devoir_poo_php_Arcade</title>
+</head>
+<body>
+
+<?php
+$orange=new orange('orange');
+$orange->affichage();
+echo '</br>';
+
+$anana= new anana('anana');
+
+$anana->affichage();
+
+?>
+    
+</body>
+</html>
